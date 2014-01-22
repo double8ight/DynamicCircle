9590dbfcd5307afd9784c3108f11cd8c8c442caa
double8ight <sevendays_@hanmail.net>
2014-01-22 10:27:29 +0900

    Dynamic Circle 게임입니다.

---
+++ DynamicCircle/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  DynamicCircle
+//
+//  Created by SDT-1 on 2014. 1. 22..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *circleImage;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
